6a36d6b8679457b2440df81f0a77f5a469c1acd5
bruno silva <bmsasilva@gmail.com>
2018-02-07 15:36:34 +0000

    Reindent lines

--- R/run_app.R
+++ R/run_app.R
@@ -6,7 +6,8 @@
 run_app <- function() {
   app_dir <- system.file("shiny-app", package = "visualHotspots")
   if (app_dir == "") {
-    stop("Could not find example directory. Try re-installing `visualHotspots`.", call. = FALSE)
+    stop("Could not find example directory.
+         Try re-installing `visualHotspots`.", call. = FALSE)
   }
   shiny::runApp(app_dir, display.mode = "normal")
 }
--- inst/shiny-app/ui.R
+++ inst/shiny-app/ui.R
@@ -2,33 +2,34 @@ shinyUI(fluidPage(
   theme = "bootstrap.css",
   fluidRow(
     column(
-      3,
+      4,
       shinyFilesButton("file_count",
                        "Choose count file",
                        "Choose count file", FALSE)
     ),
     column(
-      3,
-      shinyFilesButton("file_roads",
-                       "Choose roads file",
-                       "Choose roads file", FALSE)
+      4,
+      downloadButton("Kernel Shapefile", "Download Shapefile")
     ),
+    br(),
     br(),
     fluidRow(
       column(
-      3,
-      downloadButton("Kernel Shapefile", "Download Shapefile")
-    ),
-    column(
-      3,
-      selectInput(
-        "bandw",
-        NULL,
-        choices = c("Bandwidth 500m" = 500,
-                    "Bandwidth 1000m" = 1000),
-        selected =  500
-      )
-    )),
+        4,
+        shinyFilesButton("file_roads",
+                         "Choose roads file",
+                         "Choose roads file", FALSE)
+      ),
+      column(
+        4,
+        selectInput(
+          "bandw",
+          NULL,
+          choices = c("Bandwidth 500m" = 500,
+                      "Bandwidth 1000m" = 1000),
+          selected =  500
+        )
+      )),
     leafletOutput("mymap", height = 600)
   )
 ))
