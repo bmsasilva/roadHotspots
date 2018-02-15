c2c6d1f3cc9f821aa560160046e00a7ce3c62d38
bruno silva <bmsasilva@gmail.com>
2018-02-15 10:43:17 +0000

    Reindent lines

--- Build.R
+++ Build.R
@@ -1,7 +1,7 @@
 package.name <- "roadHotspots"
-package.dir <- paste0("~/DADOS/BRUNO/MyPackges/",package.name)
+package.dir <- paste0("~/DADOS/BRUNO/MyPackges/", package.name)
 setwd(package.dir)
 ### --- Use Roxygenise to generate .RD files from my comments
 library(roxygen2)
-roxygenise(package.dir=package.dir)
-system(command=paste("R CMD INSTALL '",package.dir,"'",sep=""))
+roxygenise(package.dir = package.dir)
+system(command = paste("R CMD INSTALL '", package.dir, "'", sep=""))
